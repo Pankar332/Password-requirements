67733f2c75d84c6707da6b78bc90b362ed651299
Pankar332 <e3637798@student.dcccd.edu>
2025-03-08 15:24:24 -0600

    Add files via upload

---
+++ Program 4A3.py
@@ -0,0 +1,25 @@
+password = input("Enter a password:")
+if len(password) < 8:
+    print("Password MUST be at least 8 characters long")
+numCount = 0
+symbolCount = 0
+specialSymbols = ['!','&','@','*','$']
+
+for char in password:
+    if char.isdigit():
+        numCount += 1
+    elif char in specialSymbols:
+        symbolCount += 1
+if len(password) >= 8:
+    if numCount == 0:
+        print("Password must have at least one number")
+if symbolCount == 0:
+    print("Password must have at least one special character from ! @ & * $")
+if numCount > 0 and symbolCount > 0:
+    print("Password meets all requirements")
+    
+   
+        
+
+    
+    
